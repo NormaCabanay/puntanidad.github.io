57820ad732fc4a0fa717b5bc0dfb2c1496cfbfe3
NormaCabanay <142052481+NormaCabanay@users.noreply.github.com>
2024-06-27 15:18:36 -0300

    armando del proyecto

---
+++ frase.js
@@ -0,0 +1,55 @@
+let citas = [
+    {
+      'autor': 'Antonio Esteban Agüero',
+      'texto': 'Una flor se abrió para mis ojos en la mañana, cuando yo me moría.'
+    },
+    {
+      'autor': 'Berta Vidal de Battini',
+      'texto': 'La lengua es el alma de un pueblo; a través de ella se manifiesta el espíritu de una raza.'
+    },
+    {
+      'autor': 'Jesús Liberato Tobares',
+      'texto': 'La poesía es el eco del alma que resuena en los corazones de los hombres.'
+    },
+    {
+      'autor': 'Alberto Rodríguez Saá',
+      'texto': 'La política es el arte de servir al pueblo, de hacer que la justicia y la igualdad sean realidades tangibles.'
+    },
+    {
+      'autor': 'José La Vía',
+      'texto': 'La fotografía es el instante capturado de la vida, una pausa en el flujo constante del tiempo.'
+    },
+    {
+      'autor': 'Dora Ochoa de Masramón',
+      'texto': 'El conocimiento es la llave que abre todas las puertas del universo.'
+    },
+    {
+      'autor': 'Edgar Morisolil',
+      'texto': 'La literatura es el refugio de los que buscan la verdad más allá de las apariencias.'
+    },
+    {
+      'autor': 'Juan Crisóstomo Lafinur',
+      'texto': 'La educación es el pilar fundamental de una sociedad justa y equitativa.'
+    },
+    {
+      'autor': 'Sara Gallardo',
+      'texto': 'La literatura es un espejo en el que la sociedad puede ver reflejadas sus más profundas verdades.'
+    },
+    {
+      'autor': 'María Delia Gatica de Montiveros',
+      'texto': 'La historia de una nación se escribe con las palabras de sus poetas y escritores.'
+    },
+    {
+      'autor': 'Susana Esther Soba',
+      'texto': 'El arte es la expresión más pura del alma humana, un puente entre lo real y lo imaginario.'
+    },
+    {
+      'autor': 'María Esther Pérez',
+      'texto': 'La poesía es la voz de los sentimientos que encuentran su cauce en las palabras.'
+    }
+    ,
+    {
+      'autor': ' Roberto Maño',
+      'texto': 'Siento que el alma se vuelve estrella, porque es mí tierra justo Daract.'
+    }
+  ];
